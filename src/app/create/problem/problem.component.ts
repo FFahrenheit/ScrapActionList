72e9f6c2abec75adb9e0af0c915a5966a0e9ca9e
Iván L. Murillo <diskman199@gmail.com>
2021-11-25 10:15:19 -0600

    Agregado D2, definición del problema

--- src/app/create/problem/problem.component.ts
+++ src/app/create/problem/problem.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/shared/alert';
 
 @Component({
   selector: 'app-problem',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProblemComponent implements OnInit {
 
-  constructor() { }
+  public form : FormGroup;
+  public options = ['Yes', 'No', 'N/A'];
+
+  constructor(private fb      : FormBuilder,
+              private alert   : AlertService,
+              private router  : Router) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      whatIs : ['', Validators.required ],
+      whereIs : ['', Validators.required ],
+      whenIs : ['', Validators.required ],
+      whoIs : ['', Validators.required ],
+      whyIs : ['', Validators.required ],
+      much : ['', Validators.required ],
+      often : ['', Validators.required ],
+      customerPN : ['', Validators.required ],
+      pnDescription : ['', Validators.required ],
+      description : ['', Validators.required ],
+      repeated : [null, Validators.required ],
+      finalAffected : [null, Validators.required ],
+      customerAffected : [null, Validators.required ],
+    });
+  }
+
+  public get(ctrl : string){
+    return this.form.controls[ctrl];
+  }
+
+  public getClass(ctrl : string) : string{
+    if (this.get(ctrl).untouched){
+      return ''
+    };
+    return this.get(ctrl).valid ? 'is-valid' : 'is-invalid';
+  }
+
+  public continue() : void{
+    if(this.form.valid){
+      this.submit();
+    }else{
+      this.form.markAllAsTouched();
+    }
+  }
+
+  public submit() : void{
+    console.log('Wait...');
   }
 
 }
